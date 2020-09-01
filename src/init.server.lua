9ac7d1c9e93068aa0d2ff0cd885c9b7bdc3fcf55
rwilliaise <rajawilliams24@gmail.com>
2020-09-01 13:05:20 -0700

    Add documentation
    Github Actions FTW

---
+++ src/init.server.lua
@@ -0,0 +1,11 @@
+local Toolbar = plugin:CreateToolbar("Filmmaker")
+local OpenButton = Toolbar:CreateButton("Open Filmmaker", "Open Filmmaker", "rbxassetid://0")
+
+local anim = script:WaitForChild("anim")
+local Animation = require(amim:WaitForChild("Animation.lua"))
+local ik = script:WaitForChild("ik")
+local ui = script:WaitForChild("ui")
+
+OpenButton.Click:Connect(function()
+
+end)
