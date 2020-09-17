6adbb52cd6a494d92d1e1d4d337a0c6b2d3245e5
rwilliaise <rajawilliams24@gmail.com>
2020-09-17 07:52:31 -0700

    Rewrite how saving works
    This is not standalone

--- src/Data/Rig.ts
+++ src/Data/Rig.ts
@@ -4,17 +4,59 @@ enum RigType {
 	Custom = 3,
 }
 
-interface Rig {
+/**
+ * Rig is an umbrella class that lays out how a rig should be loaded, and how to
+ * save it into a usable format.
+ * 
+ * @see FBXRig
+ */
+export abstract class Rig {
+	type: RigType;
 
+	/**
+	 * A base constructor for the rig class.
+	 * @param rigType The RigType to use.
+	 */
+	constructor(rigType: RigType = RigType.R15) {
+		this.type = rigType;
+	}
 	
+	abstract save(): Instance;
+	abstract load(): void;
 }
 
-class FBXRig implements Rig {
-	
+export class FBXRig extends Rig {
 
-	
+	constructor(rigType: RigType = RigType.R15) {
+		if (rigType === RigType.Custom) {
+			warn("Due to the limitations of FBX loading, it is currently impossible to load in custom rigs. Sorry!");
+			rigType = RigType.R15;
+		}
+		super(rigType);
+	}
+
+	save(): Instance {
+		throw "Method not implemented.";
+	}
+
+	load(): void {
+		let rig = plugin.ImportFbxRig(this.type === RigType.R15);
+		rig.Parent = undefined;
+		if (!rig.FindFirstChildWhichIsA("Motor6D", true)) {
+
+		}
+	}
 }
 
+export class GeneratedRig extends Rig {
 
+	save(): Instance {
+		throw "Method not implemented.";
+	}
 
-export {};
+	load(): void {
+		
+
+	}
+
+}
